c51fab1ec95d088f9efc6fc664147b900225302b
mofath <mohamed.elgaedi@gmail.com>
2021-05-03 22:11:58 +0200

    Display components in docs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Docs from './docs/Docs';
 import "./index.scss";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Docs />, document.getElementById("root"));
